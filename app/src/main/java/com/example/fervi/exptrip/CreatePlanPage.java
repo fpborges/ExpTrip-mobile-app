d0395d7f9052a1bbe7345394f1d4579a472e498a
Fernando <fpborges1981@gmail.com>
2017-10-03 14:57:24 -0400

    change layout to landscape orientation

--- app/src/main/java/com/example/fervi/exptrip/CreatePlanPage.java
+++ app/src/main/java/com/example/fervi/exptrip/CreatePlanPage.java
@@ -1,7 +1,9 @@
 package com.example.fervi.exptrip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreatePlanPage extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class CreatePlanPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_plan_page);
+    }
+    public void sendCreate2(View view) {
+        Intent intent = new Intent(this, CreatePlan2.class);
+        startActivity(intent);
     }
 }
